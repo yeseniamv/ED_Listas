593c1fcf19ea8c1e7b59f805e5169e37371c7d8a
Yesenia <yeseniamv1995@gmail.com>
2023-02-25 09:18:56 -0700

    se hace una copia de listas simplemente ligadas para hacer la lista circular

---
+++ ListasCirculares/Lista.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasCirculares
+{
+    internal class Lista
+    {
+        Nodo nodoInicial;
+        Nodo nodoActual;
+        public Lista()
+        {
+            nodoInicial = new Nodo();
+        }
+        public bool ValidaVacio()
+        {
+            if (nodoInicial.Siguiente == null)
+            {
+                return true;
+            }
+            return false;
+            //return nodoInicial.Siguiente == null;
+
+        }
+        public void VaciarLista()
+        {
+            nodoInicial.Siguiente = null;
+        }
+        public string Recorrer()
+        {
+            string datos = string.Empty;
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                datos += nodoActual.Valor + "\n";
+            }
+            return datos;
+        }
+        public void Agregar(string valor)
+        {
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+            }
+            Nodo nodoNuevo = new Nodo(valor);
+            nodoActual.Siguiente = nodoNuevo;
+        }
+        public Nodo Buscar(string valor)
+        {
+            if (ValidaVacio())
+            {
+                return null;
+            }
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                if (nodoActual.Valor == valor)
+                {
+                    return nodoActual;
+                }
+
+            }
+            return null;
+        }
+
+        public void AgregarInicio(string valor)
+        {
+            //creamos nuevo nodo
+
+            nodoActual = nodoInicial;
+            Nodo nodoNuevo = new Nodo(valor);
+            //conectamos nuevo nodo al primer nodo de la lista
+            nodoNuevo.Siguiente = nodoActual.Siguiente;
+            //conectamos nodo inicial con nodo nuevo
+            nodoInicial.Siguiente = nodoNuevo;
+
+
+        }
+
+
+    }
+
+}
---
+++ ListasCirculares/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasCirculares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nodo nodo = new Nodo();
+            Lista list = new Lista();
+
+
+            //agregamos elementos
+            list.Agregar("Primero");
+            list.Agregar("Segundo");
+            list.Agregar("Tercero");
+
+            //probamos recorrer
+            Console.WriteLine(list.Recorrer());
+            Console.ReadKey();
+
+            //buscamos un valor y mostramos su posicion
+            Console.WriteLine(list.Buscar("Primero"));
+            Console.ReadKey();
+
+            //agregamos al inicio
+            list.AgregarInicio("Nuevo primero");
+            Console.WriteLine(list.Recorrer());
+            Console.ReadKey();
+
+        }
+    }
+
+}
